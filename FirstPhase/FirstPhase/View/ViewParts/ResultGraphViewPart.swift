f0b1f0d3bdf66e28cb609d1c08569be8aa1c70ad
hor1guch1ke1suke <hor1guch1ke1suke@yahoo.co.jp>
2021-05-11 14:33:24 +0900

    グラフのMock-up作成

--- FirstPhase/FirstPhase/View/ViewParts/ResultGraphViewPart.swift
+++ FirstPhase/FirstPhase/View/ViewParts/ResultGraphViewPart.swift
@@ -11,10 +11,31 @@ import SwiftUI
 struct ResultGraphViewPart: Shape {
     
     func path(in rect: CGRect) -> Path {
-        Path { path in
-            path.addEllipse(in: CGRect(x: 0, y: 0, width: rect.width, height: rect.height))
+        let points = [
+            //グラデーションレイヤー入れたいから始点と終点を0として設定しておく。
+            CGPoint(x: rect.width * 0.05 , y: rect.height * 0.5),
+            CGPoint(x: rect.width * 0.1 , y: rect.height * 0.5 * (1.0 - 0.3)),
+            CGPoint(x: rect.width * 0.225 , y: rect.height * 0.5 * (1.0 - 0.5)),
+            CGPoint(x: rect.width * 0.35 , y: rect.height * 0.5 * (1.0 - 0.8)),
+            CGPoint(x: rect.width * 0.475 , y: rect.height * 0.5 * (1.0 - 0.4)),
+            CGPoint(x: rect.width * 0.6 , y: rect.height * 0.5 * (1.0 - 0.6)),
+            CGPoint(x: rect.width * 0.725 , y: rect.height * 0.5 * (1.0 - 0.2)),
+            CGPoint(x: rect.width * 0.85 , y: rect.height * 0.5 * (1.0 - 0.9)),
+            CGPoint(x: rect.width * 0.9 , y: rect.height * 0.5)
             
+        ]
+        return Path { path in
+            path.addLines(points)
         }
+        
     }
     
+}
+
+struct ResultGraphViewPart_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultGraphViewPart()
+            .fill(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]), startPoint: .top, endPoint: .center))
+            .shadow(color: .gray, radius: 5, x: 2.0, y: -10)
+    }
 }
