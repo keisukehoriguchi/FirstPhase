524ce8a6a6ee01b2898896e032e915b3266363f0
hor1guch1ke1suke <hor1guch1ke1suke@yahoo.co.jp>
2021-05-12 13:22:39 +0900

    カレンダーロジックをわかりやすく記載

--- FirstPhase/FirstPhase/Model/CalendarLogic.swift
+++ FirstPhase/FirstPhase/Model/CalendarLogic.swift
@@ -9,164 +9,81 @@ import Foundation
 
 struct CalendarLogic {
     
+    //閏年算出公式
     let isLeapYear = { (year: Int) in year % 400 == 0 || (year % 4 == 0 && year % 100 != 0) }
+    //曜日の算出公式
     let zellerCongruence = { (year: Int, month: Int, day: Int) in (year + year/4 - year/100 + year/400 + (13 * month + 8)/5 + day) % 7 }
     
-    private func dateManager(year: Int, month: Int) -> [Int] {
-        let firstDayOfWeek = dayOfWeek(year, month, 1)
-        let numberOfCells = numberOfWeeks(year, month) * 7
-        let days = numberOfDays(year, month)
-        let Array = alignmentOfDays(firstDayOfWeek, numberOfCells, days)
+    //年と月の指定で[Int]型として月の日付を返す。先月と来月の日付は0として返している。
+    func dateManager(year: Int, month: Int) -> [Int] {
+        let firstDayOfWeek = dayOfWeek(year, month, 1) //月の初めが何曜日か算出
+        let numberOfCells = numberOfWeeks(year, month) * 7 //その月が何週間あるか算出
+        let days = numberOfDays(year, month) //その月が何日あるか算出
+        let Array = alignmentOfDays(firstDayOfWeek, numberOfCells, days) //表示しやすいように[Int]型に整形
         return Array
     }
     
-        func numberOfWeeks(year: Int, month: Int) -> [String] {
-            var numberOfWeeksArray: [String] = []
-            for item in 0 ..< numberOfWeeks {
-                let toStrig = String(item)
-                numberOfWeeksArray.append(toStrig)
-            }
-            return numberOfWeeksArray
-        }
-    
-        func updateCalendar(y:Int, m:Int) {
-            dateManager(year: y, month: m)
-            numberOfWeeks(year: y, month: m)
-        }
-    
-        func backToToday(){
-            monthCounter = 0
-            getToday()
-            updateCalendar(y: thisYear, m: thisMonth)
-        }
-    
-    
 }
 
-//
-//class CalendarUseCase: ObservableObject {
-//
-//    init() {
-//        getToday()
-//        updateCalendar(y: thisYear, m: thisMonth)
-//    }
-//
-//    @Published var thisYear: Int = 0
-//    @Published var thisMonth: Int = 0
-//    @Published var selectedDay:Int = 0
-//    @Published var today: Int = 0
-//
-//    var monthCounter = 0 {
-//        didSet{
-//            let date = DateItems.MoveMonth.Request(monthCounter)
-//            thisYear = date.year
-//            thisMonth = date.month
-//            selectedDay = 1
-//            updateCalendar(y: thisYear, m: thisMonth)
-//        }
-//    }
-//
-//    @Published var numberOfWeeks: Int = 0
-//    @Published var daysArray: [Int] = [1,2]
-//    @Published var numberOfWeeksArray:[String] = []
-//
-//    private let date = DateItems.ThisMonth.Request()
-//
-//    let isLeapYear = { (year: Int) in year % 400 == 0 || (year % 4 == 0 && year % 100 != 0) }
-//    let zellerCongruence = { (year: Int, month: Int, day: Int) in (year + year/4 - year/100 + year/400 + (13 * month + 8)/5 + day) % 7 }
-//
-//    func dateManager(year: Int, month: Int){
-//        let firstDayOfWeek = dayOfWeek(year, month, 1)
-//        let numberOfCells = numberOfWeeks(year, month) * 7
-//        let days = numberOfDays(year, month)
-//        let Array = alignmentOfDays(firstDayOfWeek, numberOfCells, days)
-//        daysArray = Array
-//    }
-//
-//    func numberOfWeeks(year: Int, month: Int) {
-//        let weeks = numberOfWeeks(year, month)
-//        numberOfWeeks = weeks
-//
-//        numberOfWeeksArray = []
-//        for item in 0 ..< numberOfWeeks {
-//            let toStrig = String(item)
-//            numberOfWeeksArray.append(toStrig)
-//        }
-//    }
-//
-//    func updateCalendar(y:Int, m:Int) {
-//        dateManager(year: y, month: m)
-//        numberOfWeeks(year: y, month: m)
-//    }
-//
-//    func backToToday(){
-//        monthCounter = 0
-//        getToday()
-//        updateCalendar(y: thisYear, m: thisMonth)
-//    }
-//
-//}
-//
-//extension CalendarUseCase {
-//
-//    private func dayOfWeek(_ year: Int, _ month: Int, _ day: Int) -> Int {
-//        var year = year
-//        var month = month
-//        if month == 1 || month == 2 {
-//            year -= 1
-//            month += 12
-//        }
-//        return zellerCongruence(year, month, day)
-//    }
-//
-//    private func numberOfWeeks(_ year: Int, _ month: Int) -> Int {
-//        if conditionFourWeeks(year, month) {
-//            return 4
-//        } else if conditionSixWeeks(year, month) {
-//            return 6
-//        } else {
-//            return 5
-//        }
-//    }
-//
-//    private func numberOfDays(_ year: Int, _ month: Int) -> Int {
-//        var monthMaxDay = [1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31]
-//        if month == 2, isLeapYear(year) {
-//            monthMaxDay.updateValue(29, forKey: 2)
-//        }
-//        return monthMaxDay[month]!
-//    }
-//
-//    private func conditionFourWeeks(_ year: Int, _ month: Int) -> Bool {
-//        let firstDayOfWeek = dayOfWeek(year, month, 1)
-//        return !isLeapYear(year) && month == 2 && (firstDayOfWeek == 0)
-//    }
-//
-//    private func conditionSixWeeks(_ year: Int, _ month: Int) -> Bool {
-//        let firstDayOfWeek = dayOfWeek(year, month, 1)
-//        let days = numberOfDays(year, month)
-//        return (firstDayOfWeek == 6 && days == 30) || (firstDayOfWeek >= 5 && days == 31)
-//    }
-//
-//    private func alignmentOfDays(_ firstDayOfWeek: Int, _ numberOfCells: Int, _ days: Int) -> [Int] {
-//        var daysArray: [Int] = []
-//        var dayCount = 0
-//        for i in 0 ... numberOfCells {
-//            let diff = i - firstDayOfWeek
-//            if diff < 0 || dayCount >= days {
-//                daysArray.append(0)
-//            } else {
-//                daysArray.append(diff + 1)
-//                dayCount += 1
-//            }
-//        }
-//        return daysArray
-//    }
-//
-//    func getToday() {
-//        thisYear = date.year
-//        thisMonth = date.month
-//        today = date.day
-//        selectedDay = date.day
-//    }
-//}
+extension CalendarLogic {
+    
+    //曜日の確認公式
+    private func dayOfWeek(_ year: Int, _ month: Int, _ day: Int) -> Int {
+        var year = year
+        var month = month
+        if month == 1 || month == 2 {
+            year -= 1
+            month += 12
+        }
+        return zellerCongruence(year, month, day)
+    }
+    
+    //その月が何週間か確認するーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー↓
+    private func numberOfWeeks(_ year: Int, _ month: Int) -> Int {
+        if conditionFourWeeks(year, month) {
+            return 4
+        } else if conditionSixWeeks(year, month) {
+            return 6
+        } else {
+            return 5
+        }
+    }
+    
+    private func conditionFourWeeks(_ year: Int, _ month: Int) -> Bool {
+        let firstDayOfWeek = dayOfWeek(year, month, 1)
+        return !isLeapYear(year) && month == 2 && (firstDayOfWeek == 0)
+    }
+    
+    private func conditionSixWeeks(_ year: Int, _ month: Int) -> Bool {
+        let firstDayOfWeek = dayOfWeek(year, month, 1)
+        let days = numberOfDays(year, month)
+        return (firstDayOfWeek == 6 && days == 30) || (firstDayOfWeek >= 5 && days == 31)
+    }
+    //その月が何週間か確認するーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー↑
+    
+    //月毎の日数チェック
+    private func numberOfDays(_ year: Int, _ month: Int) -> Int {
+        var monthMaxDay = [1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31]
+        if month == 2, isLeapYear(year) {
+            monthMaxDay.updateValue(29, forKey: 2)
+        }
+        return monthMaxDay[month]!
+    }
+    
+    //表示しやすいように[Int]型に整形(表示しないのは0として設定)
+    private func alignmentOfDays(_ firstDayOfWeek: Int, _ numberOfCells: Int, _ days: Int) -> [Int] {
+        var daysArray: [Int] = []
+        var dayCount = 0
+        for i in 0 ... numberOfCells {
+            let diff = i - firstDayOfWeek
+            if diff < 0 || dayCount >= days {
+                daysArray.append(0)
+            } else {
+                daysArray.append(diff + 1)
+                dayCount += 1
+            }
+        }
+        return daysArray
+    }
+    
+}
---
+++ FirstPhase/FirstPhase/Model/DateItem.swift
@@ -0,0 +1,40 @@
+//
+//  DateItem.swift
+//  FirstPhase
+//
+//  Created by Keisuke Horiguchi on 2021/05/12.
+//
+
+import Foundation
+
+enum DateItems {
+    enum ThisMonth {
+        struct Request {
+            var year: Int
+            var month: Int
+            var day: Int
+            init() {
+                let calendar = Calendar(identifier: .gregorian)
+                let date = calendar.dateComponents([.year, .month, .day], from: Date())
+                year = date.year!
+                month = date.month!
+                day = date.day!
+            }
+        }
+    }
+
+    enum MoveMonth {
+        struct Request {
+            var year: Int
+            var month: Int
+            init(_ monthCounter: Int) {
+                let calendar = Calendar(identifier: .gregorian)
+                let date = calendar.date(byAdding: .month, value: monthCounter, to: Date())
+                let newDate = calendar.dateComponents([.year, .month], from: date!)
+                year = newDate.year!
+                month = newDate.month!
+            }
+        }
+    }
+
+}
