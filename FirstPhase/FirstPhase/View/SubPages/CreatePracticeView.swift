d69f2e925b8a885ffdde5e97c389a5807b160723
hor1guch1ke1suke <hor1guch1ke1suke@yahoo.co.jp>
2021-05-13 19:09:23 +0900

    Tweet作成画面およびPractice作成画面を実装

--- FirstPhase/FirstPhase/View/SubPages/CreatePracticeView.swift
+++ FirstPhase/FirstPhase/View/SubPages/CreatePracticeView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct CreatePracticeView: View {
+    @State var practiceName: String = ""
+    @State var categoryName: String = ""
+    @State var reminderIsOn: Bool = false
+    @State var reminderDate: Date = Date()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section(header: Text("実践項目を選択")) {
+                    TextField("実践すること", text: $practiceName)
+                    TextField("カテゴリー", text: $categoryName)
+                }
+                
+                Section {
+                        Toggle("リマインダーを設定する", isOn: $reminderIsOn)
+                    DatePicker("Date", selection: $reminderDate, displayedComponents: .hourAndMinute)
+                }
+                
+                
+                HStack(alignment: .center) {
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Cancel")
+                    })
+                    .frame(width: 100, height: 40, alignment: .center)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.blue, lineWidth: 1)
+                    )
+                    Spacer()
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Add")
+                    })
+                    .frame(width: 100, height: 40, alignment: .center)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.blue, lineWidth: 1)
+                    )
+                }
+                
+            }
+            .navigationTitle("実践を登録する")
+        }
     }
 }
 
--- FirstPhase/FirstPhase/View/SubPages/TweetPracticeView.swift
+++ FirstPhase/FirstPhase/View/SubPages/TweetPracticeView.swift
@@ -9,25 +9,65 @@ import SwiftUI
 
 struct TweetPracticeView: View {
     @State var note: String = ""
+    @State var startDate: Date = Date()
+    @State var finishDate: Date = Date()
     
     var body: some View {
-        Form {
-            Section(header: Text("実践項目を選択")) {
-                Text("Practice1")
-                Text("Category")
-            }
-            
-            Section(header: Text("情報を入力")) {
-                HStack {
-                    Text("時間:")
-                    Text("DatePicker?")
+        
+        NavigationView {
+            Form {
+                Section(header: Text("実践項目を選択")) {
+                    Text("Practice1")
+                    Text("Category")
                 }
-                HStack{
-                    TextField("Note:", text: $note)
+                
+                Section(header: Text("情報を入力")) {
+                    DatePicker("開始時間", selection: $startDate)
+                    DatePicker("終了時間", selection: $finishDate)
+                    HStack{
+                        Spacer()
+                        Text("実践時間:")
+                        Text("40分")
+                        Spacer()
+                    }
+                    VStack(spacing: 0){
+                        HStack{
+                            Text("Note:")
+                            Spacer()
+                        }
+                        .padding(0)
+                        TextEditor(text: $note)
+                            .lineLimit(10)
+                    }
                 }
+                
+                HStack(alignment: .center) {
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Cancel")
+                    })
+                    .frame(width: 100, height: 40, alignment: .center)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.blue, lineWidth: 1)
+                    )
+                    Spacer()
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Tweet")
+                    })
+                    .frame(width: 100, height: 40, alignment: .center)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.blue, lineWidth: 1)
+                    )
+                }
+                
             }
+            .navigationTitle("実践を記録する")
         }
-        .navigationTitle("実践を記録する")
     }
 }
 
