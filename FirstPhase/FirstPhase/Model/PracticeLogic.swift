9164368b35cc622bdcda56f12557e4627a9a7955
hor1guch1ke1suke <hor1guch1ke1suke@yahoo.co.jp>
2021-05-16 16:58:25 +0900

    PracticeViewModelの実装少し進める

--- FirstPhase/FirstPhase/Model/PracticeLogic.swift
+++ FirstPhase/FirstPhase/Model/PracticeLogic.swift
@@ -9,8 +9,9 @@ import Foundation
 
 struct PracticeLogic{
     
-    func addPracticeToViewModel(newPractice: Practice){
-        
+    func preparePracticeToAdd(title: String, category: PracticeCategory) -> Practice {
+        let practice = Practice(name: title, practiceCategory: category, isPractice: false, needsReminder: false, reminder: nil)
+        return practice
     }
     
     func addPracticeToFirestore(){
--- FirstPhase/FirstPhase/ViewModel/PracticeViewModel.swift
+++ FirstPhase/FirstPhase/ViewModel/PracticeViewModel.swift
@@ -7,6 +7,14 @@
 
 import Foundation
 
-struct PracticeViewModel {
+class PracticeViewModel: ObservableObject {
+    private let practiceLogic = PracticeLogic()
+    private var practices: [Practice] = []
+    
+    func addPractice(title: String, category: PracticeCategory) {
+        let newPractice = practiceLogic.preparePracticeToAdd(title: title, category: category)
+        practices.append(newPractice)
+    }
+    
     
 }
