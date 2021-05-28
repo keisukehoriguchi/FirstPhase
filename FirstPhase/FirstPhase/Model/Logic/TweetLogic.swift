fd6d212a79ec8d61d89b4e5cd7ddb54373291ae7
hor1guch1ke1suke <hor1guch1ke1suke@yahoo.co.jp>
2021-05-28 14:47:43 +0900

    TweetLogicの実装

--- FirstPhase/FirstPhase/Model/Logic/TweetLogic.swift
+++ FirstPhase/FirstPhase/Model/Logic/TweetLogic.swift
@@ -6,40 +6,109 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 struct TweetLogic {
     
+    private let db = Firestore.firestore()
+    var tweetPath: String = "Tweet"
+    
     func addTweetToFirestore(tweet: Tweet, _ handler: @escaping FirestoreResultHandler<[Tweet]>) {
-            //Firestoreへの保存での条件分岐
-            handler(.failure(.logic))
+        var tweets:[Tweet] = []
+        let documentRef = db.collection(tweetPath).document(tweet.tweetId.uuidString)
+        do {
+            try documentRef.setData(from: tweet)
+        } catch let error {
+            print(error.localizedDescription)
             handler(.failure(.network))
-            let tweets:[Tweet] = []
-            handler(.success(tweets))
         }
+        //全体読み込み部分
+        db.collection(tweetPath).getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print(err.localizedDescription)
+                handler(.failure(.network))
+            } else {
+                let tweet = querySnapshot?.documents.compactMap { document in
+                    return try? Firestore.Decoder().decode(Tweet.self, from: document.data())
+                }
+                tweets.append(contentsOf: tweet ?? [])
+                
+                
+                tweets.sort { $0.tweetId.uuidString > $1.tweetId.uuidString }
+                handler(.success(tweets))
+            }
+        }
+    }
     
-        func deleteTweetFromFirestore(tweet: Tweet, _ handler: @escaping FirestoreResultHandler<[Tweet]>) {
-            //Firestoreへの保存での条件分岐
-            handler(.failure(.logic))
+    func deleteTweetFromFirestore(tweet: Tweet, _ handler: @escaping FirestoreResultHandler<[Tweet]>) {
+        var tweets:[Tweet] = []
+        db.collection(tweetPath).document(tweet.tweetId.uuidString).delete()
+        
+        //全体読み込み部分
+        db.collection(tweetPath).getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print(err.localizedDescription)
+                handler(.failure(.network))
+            } else {
+                let tweet = querySnapshot?.documents.compactMap { document in
+                    return try? Firestore.Decoder().decode(Tweet.self, from: document.data())
+                }
+                tweets.append(contentsOf: tweet ?? [])
+                
+                
+                tweets.sort { $0.tweetId.uuidString > $1.tweetId.uuidString }
+                handler(.success(tweets))
+            }
+        }
+    }
+    
+    func updateTweetToFirestore(tweet: Tweet, _ handler: @escaping FirestoreResultHandler<[Tweet]>) {
+        var tweets:[Tweet] = []
+        let documentRef = db.collection(tweetPath).document(tweet.tweetId.uuidString)
+        do {
+            try documentRef.setData(from: tweet, merge: true)
+        } catch let error {
+            print(error.localizedDescription)
             handler(.failure(.network))
-            let tweets:[Tweet] = []
-            handler(.success(tweets))
-    
         }
-    
-        func updateTweetToFirestore(tweet: Tweet, _ handler: @escaping FirestoreResultHandler<[Tweet]>) {
-            //Firestoreへの保存での条件分岐
-            handler(.failure(.logic))
-            handler(.failure(.network))
-            let tweets:[Tweet] = []
-            handler(.success(tweets))
+        //全体読み込み部分
+        db.collection(tweetPath).getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print(err.localizedDescription)
+                handler(.failure(.network))
+            } else {
+                let tweet = querySnapshot?.documents.compactMap { document in
+                    return try? Firestore.Decoder().decode(Tweet.self, from: document.data())
+                }
+                tweets.append(contentsOf: tweet ?? [])
+                
+                
+                tweets.sort { $0.tweetId.uuidString > $1.tweetId.uuidString }
+                handler(.success(tweets))
+            }
         }
+        
+    }
     
-        func fetchTweetsFromFirestore( _ handler: @escaping FirestoreResultHandler<[Tweet]>) {
-            //Firestoreへの保存での条件分岐
-            handler(.failure(.logic))
-            handler(.failure(.network))
-            let tweets:[Tweet] = []
-            handler(.success(tweets))
+    func fetchTweetsFromFirestore( _ handler: @escaping FirestoreResultHandler<[Tweet]>) {
+        var tweets:[Tweet] = []
+        //全体読み込み部分
+        db.collection(tweetPath).getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print(err.localizedDescription)
+                handler(.failure(.network))
+            } else {
+                let tweet = querySnapshot?.documents.compactMap { document in
+                    return try? Firestore.Decoder().decode(Tweet.self, from: document.data())
+                }
+                tweets.append(contentsOf: tweet ?? [])
+                
+                
+                tweets.sort { $0.tweetId.uuidString > $1.tweetId.uuidString }
+                handler(.success(tweets))
+            }
         }
+    }
     
 }
---
+++ FirstPhase/FirstPhaseTests/TweetLogicTest.swift
@@ -0,0 +1,101 @@
+
+//import XCTest
+//@testable import FirstPhase
+//import FirebaseFirestore
+//import FirebaseFirestoreSwift
+//
+//final class TweetLogicTest: XCTestCase {
+//    
+//    var tweetLogic = TweetLogic()
+//    
+////    override func setUpWithError() throws {
+////        // Put setup code here. This method is called before the invocation of each test method in the class.
+////        practiceLogic.practicePath = "PracticeTest"
+////        let db = Firestore.firestore()
+////
+////        let documentRef = db.collection(practiceLogic.practicePath).document(practice1.practiceId.uuidString)
+////        do {
+////            try documentRef.setData(from: practice1)
+////        } catch {
+////            XCTFail()
+////        }
+////    }
+////
+////    override func tearDownWithError() throws {
+////        // Put teardown code here. This method is called after the invocation of each test method in the class.
+////    }
+//    
+//    func testFetch() throws {
+//        let exp: XCTestExpectation = expectation(description: "wait for finish")
+//        practiceLogic.fetchPracticesFromFirestore { result in
+//            switch result {
+//            case .failure(let error):
+//                print(error.localizedDescription)
+//                XCTFail()
+//            case .success(let practices):
+//                XCTAssertEqual([practice1], practices)
+//                exp.fulfill()
+//            }
+//        }
+//        wait(for: [exp], timeout: 5)
+//    }
+//    
+//    func testAdd() throws {
+//        let exp: XCTestExpectation = expectation(description: "wait for finish")
+//        practiceLogic.addPracticeToFirestore(practice: practice2    ) { result in
+//            switch result {
+//            case .failure(let error):
+//                print(error.localizedDescription)
+//                XCTFail()
+//            case .success(let practices):
+//                XCTAssertEqual([practice1, practice2], practices)
+//                exp.fulfill()
+//            }
+//        }
+//        wait(for: [exp], timeout: 5)
+//    }
+//    
+//    func testUpdate() throws {
+//        let exp: XCTestExpectation = expectation(description: "wait for finish")
+//        practiceLogic.updatePracticeToFirestore(practice: practice2_1) { result in
+//            switch result {
+//            case .failure(let error):
+//                print(error.localizedDescription)
+//                XCTFail()
+//            case .success(let practices):
+//                XCTAssertEqual([practice1, practice2_1], practices)
+//                exp.fulfill()
+//            }
+//        }
+//        wait(for: [exp], timeout: 5)
+//    }
+//    
+//    func testDelete() throws {
+//        let exp: XCTestExpectation = expectation(description: "wait for finish")
+//        practiceLogic.deletePracticeFromFirestore(practice: practice2_1) { result in
+//            switch result {
+//            case .failure(let error):
+//                print(error.localizedDescription)
+//                XCTFail()
+//            case .success(let practices):
+//                XCTAssertEqual([practice1], practices)
+//                exp.fulfill()
+//            }
+//        }
+//        wait(for: [exp], timeout: 5)
+//    }
+//}
+//
+//
+////
+////    func testExample() throws {
+////        // This is an example of a functional test case.
+////        // Use XCTAssert and related functions to verify your tests produce the correct results.
+////    }
+////
+////    func testPerformanceExample() throws {
+////        // This is an example of a performance test case.
+////        self.measure {
+////            // Put the code you want to measure the time of here.
+////        }
+////    }
